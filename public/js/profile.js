2631c4c5c14b124ae04f57667f5d4acd0c1493ba
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-01-13 13:03:12 -0800

    Integrating Semantic UI to help with search functionality

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,3 +3,5 @@ var wateringCalendar = document.getElementById('wateringCalendar');
 if(wateringCalendar) {
 	wateringCalendar.valueAsDate = new Date();
 }
+
+$('.dropdown').dropdown({ transition: 'drop' });
