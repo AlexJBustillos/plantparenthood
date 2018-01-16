fbe6714d68387575b965e33b57a082c5806a81c9
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-01-16 00:57:41 -0800

    Trying to resolve dupes

---
+++ dbSetup.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var flash = require('connect-flash');
+var isLoggedIn = require('../middleware/loggedin');
+var session = require('express-session');
+var passport = require('../config/passportConfig');
+var db = require('../models');
+var request = require('request');
+var cheerio = require('cheerio');
+var router = express.Router();
+
+
+for (var i = 229; i <= 456; i++) {
+	var currentPlantId = i;
+	db.comment.destroy({
+    where: { 
+    	id: currentPlantId
+    }
+  }).then(function(deleted){
+    console.log("Plant deleted",deleted);
+}
+
+module.exports = router;
