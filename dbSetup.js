7d5d99c168ce2f05d92804f194aaafa6dca75038
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-01-16 01:00:56 -0800

    -_____-

--- dbSetup.js
+++ dbSetup.js
@@ -1,11 +1,6 @@
 require('dotenv').config();
 var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var flash = require('connect-flash');
-var isLoggedIn = require('../middleware/loggedin');
-var session = require('express-session');
-var passport = require('../config/passportConfig');
-var db = require('../models');
+var db = require('./models');
 var request = require('request');
 var cheerio = require('cheerio');
 var router = express.Router();
