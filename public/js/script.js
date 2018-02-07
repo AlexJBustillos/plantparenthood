08810e036cfba5db58cc177b7437d0fe6b4d9f30
jacquelynmarcella <jacquelyn.marcella@gmail.com>
2018-02-06 21:17:15 -0800

    Responsive navbar working

--- public/js/script.js
+++ public/js/script.js
@@ -16,6 +16,19 @@ $('.dropdown').dropdown({
 	match: 'text'
 });
 
-$(".nav-hamburger").click(function(){
-	$(".desktop-nav").toggle("blind", 500);
+// Responsive Navbar
+$(".nav-hamburger").click(function(event){
+	event.preventDefault();
+	$(".nav-responsive").toggle("blind", 500);
 });
+
+// The below 2 functions hide the dropdown if it is expanded when the screen is small, but then the user makes their screen large 
+$(window).resize(function(){
+    checkSize();
+});
+
+function checkSize(){
+    if ($(".nav-hamburger").css("display") == "none" ){
+        $(".nav-responsive").hide();
+    }
+}
